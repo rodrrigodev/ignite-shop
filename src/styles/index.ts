f08aa79eb26b88e4937a9b8e2527e8a66c6a5821
Rodrigo <rodrigosanyy@gmail.com>
2023-04-21 10:28:27 -0300

    home base

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -11,6 +11,13 @@ export const {config, css, keyframes, styled, globalCss, getCssText, createTheme
 
             green500: '#00875f',
             green300: '#00b37e'
+        },
+
+        fontSizes: {
+            md: '1.125rem',
+            lg: '1.25rem',
+            xl: '1.5rem',
+            '2xl': '2rem',
         }
     }
 })
