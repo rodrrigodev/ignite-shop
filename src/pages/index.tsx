add90881df3588f0386404e32af00c087310d875
Rodrigo <rodrigosanyy@gmail.com>
2023-04-25 09:36:07 -0300

    add static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image"
-import { HomeContainer, Product } from "./home"
+import { HomeContainer, Product } from "../styles/pages/home"
 import {useKeenSlider} from 'keen-slider/react'
-
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps{
@@ -45,7 +41,7 @@ export default function Home({products}: HomeProps) {
  )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const response = await stripe.products.list({
         expand: ['data.default_price']
     })
@@ -64,6 +60,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return{
         props: {
            products
-        }
+        },
+        revalidate: 60 * 60 * 2, // a casa 2h tudo sera recarregado novamente
     }
 }
