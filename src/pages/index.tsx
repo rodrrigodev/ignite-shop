3d5fde7964cc53e8475efe5ecf2e237b9d83ab9e
Rodrigo <rodrigosanyy@gmail.com>
2023-04-26 17:12:15 -0300

    fallback SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,13 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
 import { GetStaticProps } from "next"
 import Stripe from "stripe"
-import Link from "next/link"
 
 interface HomeProps{
    products:{ 
     id: string,
     name: string,
     imageUrl: string,
-    price: number,
+    price: string,
     }[]
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,20 +1,38 @@
-import { useRouter } from "next/router"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import { GetStaticPaths, GetStaticProps } from "next"
+import { stripe } from "../../lib/stripe"
+import Stripe from "stripe"
+import Image from "next/image"
+import { useRouter } from "next/router"
 
-export default function Product(){
-    const { query } = useRouter()
+interface ProductProps{
+  product:{ 
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string,
+    description: string
+    }
+}
+
+export default function Product({ product }: ProductProps){
+  const { isFallback } = useRouter()
+
+  if(isFallback){
+    return <p>Loading...</p>
+  }
 
     return(
       <ProductContainer>
         <ImageContainer>
-
+            <Image src={product.imageUrl} width={520} height={480} alt=""/>
         </ImageContainer>
 
         <ProductDetails>
-            <h1>Camiseta X</h1>
-            <span>R$ 79,00</span>
+            <h1>{product.name}</h1>
+            <span>{product.price}</span>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, possimus voluptates. Sapiente, sed nemo fugiat commodi et soluta quod ullam iusto distinctio neque unde tenetur blanditiis vel laudantium illo nostrum!</p>
+            <p>{product.description}</p>
 
             <button>
                 Comprar agora
@@ -22,4 +40,37 @@ export default function Product(){
         </ProductDetails>
       </ProductContainer>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async ()=>{
+  return {
+    paths: [],
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({params})=>{
+  const productId = params.id
+    
+    const product = await stripe.products.retrieve(productId, {
+      expand: ['default_price'],
+    });
+
+    const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR',{
+            style: 'currency',
+            currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1 //save html in cache for 1 hour
+  }
 }
