1fdbadb5460788f75a6d760814ee0b16b7323483
Rodrigo <rodrigosanyy@gmail.com>
2023-04-26 13:50:35 -0300

    product page html + css

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product(){
     const { query } = useRouter()
 
     return(
-        <h1>Product:{JSON.stringify(query)}</h1>
+      <ProductContainer>
+        <ImageContainer>
+
+        </ImageContainer>
+
+        <ProductDetails>
+            <h1>Camiseta X</h1>
+            <span>R$ 79,00</span>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, possimus voluptates. Sapiente, sed nemo fugiat commodi et soluta quod ullam iusto distinctio neque unde tenetur blanditiis vel laudantium illo nostrum!</p>
+
+            <button>
+                Comprar agora
+            </button>
+        </ProductDetails>
+      </ProductContainer>
     )
 }
